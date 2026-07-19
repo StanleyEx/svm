c74fcd7506724efc9e1a5daa935ca94c0c1ae896
Stanley <fagraea@outlook.com>
2026-07-19 21:39:24 +0800

    [LIR] Declare LIR Passes

--- src/LIR/LIRPass.h
+++ src/LIR/LIRPass.h
@@ -8,6 +8,11 @@
 namespace svm::ir {
 
 DECLARE_MODULE_PASS(HIRToLIRPass);
+DECLARE_MODULE_PASS(AggressiveConstFoldPass);
+DECLARE_MODULE_PASS(GlobalMergePass);
+DECLARE_MODULE_PASS(DeadArgumentEliminationPass);
+DECLARE_MODULE_PASS(DeadFunctionEliminationPass);
+DECLARE_MODULE_PASS(GlobalVariableLocalizationPass);
 DECLARE_FUNCTION_PASS(Mem2RegPass);
 DECLARE_FUNCTION_PASS(ShortCircuitCanonicalizePass);
 DECLARE_FUNCTION_PASS(SwitchCanonicalizePass);
@@ -20,6 +25,31 @@ DECLARE_FUNCTION_PASS(GCMPass);
 DECLARE_FUNCTION_PASS(LICMPass);
 DECLARE_FUNCTION_PASS(JumpThreadingPass);
 DECLARE_FUNCTION_PASS(IfConversionPass);
+DECLARE_FUNCTION_PASS(SROAPass);
+
+class InlinePass final : public ModulePass {
+public:
+  explicit InlinePass(i32 instructionThreshold = 256) noexcept;
+  std::string_view name() const noexcept override;
+  PassResult run(Module *module, PassContext &context) override;
+
+private:
+  i32 instructionThreshold_ = 256; // 内联的指令数阈值
+};
+
+class FunctionSpecializationPass final : public ModulePass {
+public:
+  explicit FunctionSpecializationPass(
+      u32 cloneLimit = 8, u32 cloneInstructionLimit = 2000,
+      u32 uniformCallSiteThreshold = 2) noexcept;
+  std::string_view name() const noexcept override;
+  PassResult run(Module *module, PassContext &context) override;
+
+private:
+  u32 cloneLimit_ = 8;               // 单次运行最多创建的clone数
+  u32 cloneInstructionLimit_ = 2000; // 可复制函数的最大指令数
+  u32 uniformCallSiteThreshold_ = 2; // uniform tuple的最少调用点数
+};
 
 class InstCombinePass final : public FunctionPass {
 public:
